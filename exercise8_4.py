cbb6f5cbb81978c244946e75aece9ecfd7fbd1e6
Jamison Lahman <lahmanja@protonmail.com>
2017-05-31 18:48:54 -0400

    Add files via upload

--- exercise8_4.py
+++ exercise8_4.py
@@ -1,8 +1,18 @@
 """
-Exercise  8.3: Download a copy of the file from www.py4e.com/code3/romeo.txt
+Exercise  8.4: Download a copy of the file from www.py4e.com/code3/romeo.txt
 
 Write a program to open the file romeo.txt and read it line by line. For each
-line, split the line into a list of words using the split function. 
+line, split the line into a list of words using the split function.
+
+For each word, check to see if the word is already in a list. If the word is
+not in the list, add it to the list.
+
+When the program completes, sort and print the resulting words in alphabetical
+order.
+
+['Arise', 'But', 'It', 'Juliet', 'Who', 'already', 'and', 'breaks', 'east', 
+'envious', 'fair', 'grief', 'is', 'kill', 'light', 'moon', 'pale', 'sick', 
+'soft', 'sun', 'the', 'through', 'what', 'window', 'with', 'yonder']
 
 Python for Everybody: Exploring Data Using Python 3
 by Charles R. Severance
---
+++ exercise8_5.py
@@ -0,0 +1,42 @@
+"""
+Exercise  8.5: Write a program to read through the mail box data and when you
+find the line that starts with "From", you will split the line into words
+using the split function. We are interested in who sent the message, which is
+second word on the From line.
+
+From stephen.marquard@uct.ac.za Sat Jan 5 09:14:16 2008
+
+You will parse the From line and print out the second word for each From line,
+then you will also count the number of From (not From:) lines and print out a
+count at the end.
+
+This is a good sample output with a few lines removed:
+
+python fromcount.py
+Enter a file name: mbox-short.txt
+stephen.marquard@uct.ac.za
+louis@media.berkeley.edu
+zqian@umich.edu
+
+[... some output removed...]
+
+ray@media.berkeley.edu
+cwen@iupui.edu
+cwen@iupui.edu
+cwen@iupui.edu
+There were 27 lines in the file with From as the first word
+
+Python for Everybody: Exploring Data Using Python 3
+by Charles R. Severance
+
+Solution by Jamison Lahman, May 31, 2017
+"""
+
+fhand = open('mbox-short.txt')
+count = 0
+for line in fhand:
+	words = line.split()
+	if len(words) == 0 or len(words) < 2 or words[0] != 'From' : continue
+	print(words[1])
+	count = count + 1
+print('There were %d lines in the file with From as the first word' % count)
