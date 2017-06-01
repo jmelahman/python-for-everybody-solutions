b7b5ba4be5039cf9384c25d6faac870b8e0a28ed
Jamison Lahman <lahmanja@protonmail.com>
2017-06-01 00:38:29 -0400

    Add files via upload

---
+++ Exercise9_2.py
@@ -0,0 +1,39 @@
+"""
+Exercise  9.2: Write a program that categorizes each mail message by which day
+of the week the commit was done. To do this, look for lines that start with 
+"From", then look for the third word and keep a running count of each of the
+days of the week. At the end of the program, print out the contents of your
+dictionary (order does not matter).
+
+Sample Line: From stephen.marquard@uct.ac.za Sat Jan 5 09:14:16 2008
+
+Sample Execution:
+python dow.py
+Enter a file name: mbox-short.txt
+{'Fri': 20, 'Thu': 6, 'Sat': 1}
+
+Python for Everybody: Exploring Data Using Python 3
+by Charles R. Severance
+
+Solution by Jamison Lahman, May 31, 2017
+"""
+
+dictionary_days = dict()                       #Initializes the dictionary
+fname = input('Enter a file name: ')
+try:
+    fhand = open(fname)
+except:
+    print('File cannot be opened:', fname)
+    exit()
+    
+for line in fhand:
+    words = line.split()
+    if len(words) == 0 or len(words) < 2 or words[0] != 'From': 
+        continue
+    else:
+        if words[2] not in dictionary_days:
+            dictionary_days[words[2]] = 1       #First entry
+        else:
+            dictionary_days[words[2]] += 1      #Additional counts
+            
+print(dictionary_days)
---
+++ exercise9_3.py
@@ -0,0 +1,37 @@
+"""
+Exercise  9.3: Write a program to read through a mail log, build a histogram
+using a dictionary to count how many messages have come from each email
+address, and print the dictionary.
+
+Enter file name: mbox-short.txt
+{'stephen.marquard@uct.ac.za': 2, 'louis@media.berkeley.edu': 3, 
+'zqian@umich.edu': 4, 'rjlowe@iupui.edu': 2, 'cwen@iupui.edu': 5, 
+'gsilver@umich.edu': 3, 'wagnermr@iupui.edu': 1, 
+'antranig@caret.cam.ac.uk': 1, 'gopal.ramasammycook@gmail.com': 1, 
+'david.horwitz@uct.ac.za': 4, 'ray@media.berkeley.edu': 1}
+
+Python for Everybody: Exploring Data Using Python 3
+by Charles R. Severance
+
+Solution by Jamison Lahman, May 31, 2017
+"""
+
+dictionary_addresses = dict()                       #Initializes the dictionary
+fname = input('Enter file name: ')
+try:
+    fhand = open(fname)
+except:
+    print('File cannot be opened:', fname)
+    exit()
+    
+for line in fhand:
+    words = line.split()
+    if len(words) == 0 or len(words) < 2 or words[0] != 'From': 
+        continue
+    else:
+        if words[1] not in dictionary_addresses:
+            dictionary_addresses[words[1]] = 1       #First entry
+        else:
+            dictionary_addresses[words[1]] += 1      #Additional counts
+
+print(dictionary_addresses)
