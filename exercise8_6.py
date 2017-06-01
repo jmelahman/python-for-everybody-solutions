c1f75fdc01a3d2ec29f0fed0a8ff4cdd541496f2
Jamison Lahman <lahmanja@protonmail.com>
2017-05-31 23:35:36 -0400

    Update exercise8_6.py

--- exercise8_6.py
+++ exercise8_6.py
@@ -1,5 +1,5 @@
 """
-Exercise  8.5: Rewrite the program that prompts the user for a list of numbers
+Exercise  8.6: Rewrite the program that prompts the user for a list of numbers
 and prints out the maximum and minimum of the numbers at the end when the user
 enters "done". Write the program to store the numbers the user enters in a list
 and use the max() and min() functions to compute the maximum and minimum 
