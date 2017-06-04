81c1ae9552cbb859e594c30227412bbdc9b0f3f2
Jamison Lahman <lahmanja@protonmail.com>
2017-06-04 02:01:02 -0400

    Add files via upload

---
+++ exercise11_1.py
@@ -0,0 +1,37 @@
+"""
+Exercise  10.3: Write a simple program to simulate the operation of the grep
+command on Unix. Ask the user to enter a regular expression and count the
+number of lines that matched the regular expression:
+
+$ python grep.py
+Enter a regular expression: ^Author
+mbox.txt had 1798 lines that matched ^Author
+
+$ python grep.py
+Enter a regular expression: ^X-
+mbox.txt had 14368 lines that matched ^X-
+
+$ python grep.py
+Enter a regular expression: java$
+mbox.txt had 4218 lines that matched java$
+
+Python for Everybody: Exploring Data Using Python 3
+by Charles R. Severance
+
+Solution by Jamison Lahman, June 4, 2017
+"""
+import re
+
+input_exp = str(input('Enter a regular expression: '))
+reg_exp = str(input_exp)                #Regular Expressions are strings
+fname = 'mbox.txt'
+fhand = open(fname)
+count = 0
+
+for line in fhand:
+    line = line.rstrip()
+    if re.findall(reg_exp,line) != []:  #Only counts if something was found
+        count += 1
+        
+print(fname,'had ',count,'lines that matched',reg_exp)
+    
---
+++ exercise11_2.py
@@ -0,0 +1,43 @@
+"""
+Exercise  10.3: Write a program to look for lines of the form
+
+'New Revision: 39771'
+
+and extract the number from each of the lines using a regular expression and
+the findall() method. Compute the average of the numbers and print out the
+average.
+
+Enter file:mbox.txt
+38549.7949721
+
+Enter file:mbox-short.txt
+39756.9259259
+
+Python for Everybody: Exploring Data Using Python 3
+by Charles R. Severance
+
+Solution by Jamison Lahman, June 4, 2017
+"""
+import re
+
+fname = input('Enter file:')
+try:
+    fhand = open(fname)
+except:
+    print('File cannot be opened:', fname)
+    exit()
+
+rev = []
+for line in fhand:
+    line = line.rstrip()
+    rev_temp = re.findall('^New Revision: ([0-9.]+)', line)
+    if rev_temp != []:
+        for val in rev_temp:
+            val = float(val)            #Convert the strings to floats
+            rev = rev + [val]           #Concats new values
+        
+rev_sum = sum(rev)
+count = float(len(rev))
+rev_ave = rev_sum / count
+
+print(rev_ave)
