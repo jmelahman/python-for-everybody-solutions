5c0dd90006eb325ba6d8f5725275e51de54e5212
dantclee <59330585+dantclee@users.noreply.github.com>
2019-12-29 22:55:27 +0800

    Update exercise12_5.py

--- exercise12_5.py
+++ exercise12_5.py
@@ -6,24 +6,23 @@ recv is receiving characters (newlines and all), not lines.
 Python for Everybody: Exploring Data Using Python 3
 by Charles R. Severance
 
-Solution by Jamison Lahman, June 5, 2017
+Solution by Daniel Lee, Dec 29, 2019
 """
 import socket
-import re
 
 my_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 my_sock.connect(('data.pr4e.org', 80))
-cmd = 'GET http://data.pr4e.org/romeo.txt HTTP/1.0\n\n'.encode()
+cmd = 'GET http://data.pr4e.org/romeo.txt HTTP/1.0\r\n\r\n'.encode()
 my_sock.send(cmd)
 
 data = my_sock.recv(512)
 message = data.decode()
 header_end_pos = message.find('\r\n\r\n') + 4   # Finds the end of header
                                             # Adds four to exclude:'\r\n\r\n'
-print(message[header_end_pos:])
+print(message[header_end_pos:],end='')
 while True:                                 # Header in the first data only
     data = my_sock.recv(512)
-    if not data:
+    if len(data) <1 :
         break
     print(data.decode())
 my_sock.close()
