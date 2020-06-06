cbb544aed830e052575aea190e045368592ca7bf
Jamison Lahman <lahmanja@protonmail.com>
2020-06-06 13:59:34 -0700

    typo

--- exercise12_3.py
+++ exercise12_3.py
@@ -1,5 +1,5 @@
 """
-Exercise  12.3: Use urllub to replicate the previous exercise of (1) retrieving
+Exercise  12.3: Use urllib to replicate the previous exercise of (1) retrieving
 the document from a URL, (2) displaying up to 3000 characters, and (3) counting
 the overall characters in the document. Don't worry about the headers for this
 exercise, simply show the first 3000 characters of the document contents.
