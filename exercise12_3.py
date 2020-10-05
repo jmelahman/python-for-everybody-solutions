7f788fd09435ef81e9f9f1c493c5985cdcb9c821
Jamison Lahman <lahmanja@protonmail.com>
2020-10-04 18:12:25 -0700

    changes from #2

--- exercise12_3.py
+++ exercise12_3.py
@@ -8,15 +8,20 @@ Python for Everybody: Exploring Data Using Python 3
 by Charles R. Severance
 """
 import urllib.request
-url = input('Enter URL: ')
-fhand = urllib.request.urlopen(url)
-count = 0
+
+
+fhand = urllib.request.urlopen('http://data.pr4e.org/romeo.txt')
+chars = 0
+char_limit = 3000
 for line in fhand:
-    sline = line.decode().strip()
-    if count + len(sline) <= 3000:
-        print(sline)
-    elif count < 3000 and count + len(sline) > 3000:
-        numchar = 3000 - count
-        print(sline[:numchar - 1])
-    count += len(sline)
-print(count)
+    # \n is considered a character; this follow commands like wc.
+    # Change the following to line.decode().rstrip('\n') if desired
+    line = line.decode()
+    next_count = chars + len(line)
+    if next_count <= char_limit:
+        print(line.rstrip('\n'))
+    elif chars < char_limit:
+        char_remain = char_limit - chars - 1
+        print(line[:char_remain])
+    chars = next_count
+print(chars)
