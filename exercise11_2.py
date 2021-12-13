00c6e117415258f1dcb43fb00c73fc677a333a27
Jamison Lahman <lahmanja@protonmail.com>
2021-12-12 22:01:32 -0800

    fix divide by zero (#22)

--- exercise11_2.py
+++ exercise11_2.py
@@ -33,13 +33,14 @@ except FileNotFoundError:
 for line in fhand:
     line = line.rstrip()
     rev_temp = re.findall('^New Revision: ([0-9.]+)', line)
-    if not rev_temp:
-        for val in rev_temp:
-            val = float(val)            # Convert the strings to floats
-            rev = rev + [val]           # Concats new values
+    for val in rev_temp:
+        val = float(val)            # Convert the strings to floats
+        rev = rev + [val]           # Combine all values
 
 rev_sum = sum(rev)
 count = float(len(rev))
-rev_ave = rev_sum / count
-
-print(rev_ave)
+if count:
+    rev_ave = rev_sum / count
+    print(rev_ave)
+else:
+    print(0)
